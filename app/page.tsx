56cfd0f208fbf7f9588ba8991cea0f6e9f3b3e7b
AjokeCode <ruqayyahalabi536@gmail.com>
2024-08-29 12:06:58 +0100

    add new product

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,12 @@
 import Header from "./components/header/header";
 import Hero from "./components/hero";
 import Product from "./components/products";
+import { useEffect } from "react";
 
 export default function Index() {
+  useEffect(() => {
+    localStorage.removeItem("product-storage");
+  }, []);
   return (
     // <main className="flex min-h-screen flex-col items-center justify-between p-24">
     <>
